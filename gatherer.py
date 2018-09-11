f872b7b81b7e658cb050ba536dd91d02b8a04b60
Stefan Matyba <stefan.matyba@sourcepark.de>
2018-09-11 08:56:24 +0200

    Parse editions from JSON, start enabling more webservices

--- gatherer.py
+++ gatherer.py
@@ -5,57 +5,31 @@ import urllib
 from pyquery import PyQuery as pq
 import pprint
 
-editions= {}
-editions["LEA"] = "Alpha"
-editions["LEB"] = "Beta"
-editions["2ED"] = "Unlimited"
-editions["CED"] = "Collectors%27+Edition"
-editions["ARN"] = "Arabian Nights"
-editions["PDRC"] = "Dragon Con"
-editions["ATQ"] = "Antiquities"
-editions["3ED"] = "Revised Edition"
-editions["LEG"] = "Legends"
-editions["SUM"] = "Summer Magic"
-editions["DRK"] = "The Dark"
-editions["FEM"] = "Fallen Empires"
-editions["4ED"] = "Fourth Edition"
-editions["ICE"] = "Ice Age"
-editions["CHR"] = "Chronicles"
-editions["REN"] = "Renaissance"
-editions["HML"] = "Homelands"
-editions["ALL"] = "Alliances"
-editions["MIR"] = "Mirage"
-editions["VIS"] = "Visions"
-editions["5ED"] = "Fifth Edition"
-editions["POR"] = "Portal"
-editions["WTH"] = "Weatherlight"
-editions["TMP"] = "Tempest"
-editions["STH"] = "Stronghold"
-editions["EXO"] = "Exodus"
-editions["UGL"] = "Unglued"
-editions["USG"] = "Urzas Saga"
-editions["ATH"] = "Anthologies"
-editions["ULG"] = "Urzas Legacy"
-editions["6ED"] = "Sixth Edition"
-editions["UDS"] = "Urzas Destiny"
+import json
+
+
+def read_editions_from_file(file):
+    with open(file, "r") as read_file:
+        data = read_file.read()
+    return data
+
+def parse_editions(json_str):
+    if json_str is None:
+        return {}
+    
+    return json.loads(json_str)
+
+editions = parse_editions(read_editions_from_file("./editions.json"))
 
 all_cards = []
 
-# Open test file
-with open('testing.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=";", )
-    first_done = False
-    for row in reader:
-        if first_done:
-            all_cards.append({ "name": row[0], "original_name": row[1], "language": row[2], "edition": row[3] })
-        first_done = True
-
-for card in all_cards:
-    print("Gathering '%s'/'%s' from '%s'..." % (card['name'], card["original_name"], editions[card['edition']]))
+def gather_cardmarket(card):
+    mkm_data = {}
+    mkm_name = editions[card['edition']]['mapping']['cardmarket']
 
     # Build URL
     fixed_name = urllib.quote_plus(card['name'])
-    fixed_edition = urllib.quote_plus(editions[card['edition']])
+    fixed_edition = urllib.quote_plus(mkm_name)
     url = "https://www.cardmarket.com/de/Magic/Products/Singles/%s/%s" % (fixed_edition, fixed_name)
     r = requests.get(url)
 
@@ -70,19 +44,45 @@ for card in all_cards:
         offset = 0
         for label in labels:
             if pq(label).text().strip() == "ab":
-                card['price'] = pq(values.eq(offset)).text()
+                mkm_data['price'] = pq(values.eq(offset)).text()
                 continue
             offset = offset+1
-            
+
+        mkm_data['rarity'] = pq(values.eq(0).find("span").eq(0)).attr('data-original-title')
+    
+    card['mkm'] = mkm_data
+    return card
+    
 
 
-        card['rarity'] = pq(values.eq(0).find("span").eq(0)).attr('data-original-title')
+# Open test file
+with open('testing.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=";", )
+    first_done = False
+    for row in reader:
+        if first_done:
+            all_cards.append({ "name": row[0], "original_name": row[1], "language": row[2], "edition": row[3] })
+        first_done = True
+
+for card in all_cards:
+
+    edition_name = editions[card['edition']]['name']
+    
+
+    print("Card: '%s'/'%s' from '%s'..." % (card['name'], card["original_name"], edition_name))
+
+    print("  -> Checking cardmarket...")
+    card = gather_cardmarket(card)
 
 
 print "*"*80
+
 for card in all_cards:
     print "Name:\t\t\t%s" % card['name']
     print "Englischer Name:\t%s" % card['original_name']
-    print "Edition:\t\t%s" % editions[card['edition']]
-    print "Preis:\t\t\t%s" % card['price'] 
+    print "Edition:\t\t%s" % editions[card['edition']]['name']
+    print "Preis [MKM]:\t\t%s" % card['mkm']['price'] 
     print "-"*40
+
+print "*"*80
+pprint.pprint(all_cards)
